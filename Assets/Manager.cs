319ddfec3791c24356269f7debe68c08a48227a1
LeMoutonZen <48433726+LeMoutonZen@users.noreply.github.com>
2019-04-23 10:26:36 +0200

    Depart

---
+++ Assets/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    private int acc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        acc = 0;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        acc += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(acc);
+    }
+}
